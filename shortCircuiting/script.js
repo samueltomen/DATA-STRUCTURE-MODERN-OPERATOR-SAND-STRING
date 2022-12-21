b36b9ca3af941880131118e3fecb267e6e7fd915
Dytsaw <tomennana.samuel@outlook.fr>
2022-12-21 17:47:06 +0100

    enhanced litteral object

--- shortCircuiting/script.js
+++ shortCircuiting/script.js
@@ -4,7 +4,23 @@
 const flights =
     '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
+    const weekdays = ['mon','tue','wed','thu','fri','sat','sun']
 // Data needed for first part of the section
+const hours = {
+    [weekdays[3]]: {
+        open: 12,
+        close: 22,
+    },
+    [weekdays[4]]: {
+        open: 11,
+        close: 23,
+    },
+    [weekdays[6]]:{
+        open: 0, // Open 24 hours
+        close: 24,
+    },
+};
+
 const restaurant = {
     name: 'Classico Italiano',
     location: 'Via Angelo Tavanti 23, Firenze, Italy',
@@ -12,25 +28,14 @@ const restaurant = {
     starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
     mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-    openingHours: {
-        thu: {
-            open: 12,
-            close: 22,
-        },
-        fri: {
-            open: 11,
-            close: 23,
-        },
-        sat: {
-            open: 0, // Open 24 hours
-            close: 24,
-        },
-    },
-    order: function (starterIndex, mainIndex) {
+    // ES6 ehanced object litteral
+     hours,
+
+    order(starterIndex, mainIndex) {
         return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
     },
 
-    orderDelivery: function ({
+    orderDelivery({
         starterIndex = 1,
         mainIndex = 0,
         time = '20:00',
@@ -41,25 +46,25 @@ const restaurant = {
         );
     },
 
-    orderPasta: function (ing1, ing2, ing3) {
+    orderPasta(ing1, ing2, ing3) {
         console.log(
             `Here is your delicious pasta with ${ing1} ${ing2} ${ing3}`
         );
     },
-    orderPizza: function (mainIngredient, ...othersIngredients) {
+    orderPizza(mainIngredient, ...othersIngredients) {
         console.log(mainIngredient);
         console.log(othersIngredients);
     },
 };
 
-// Boucle for of
-const menu = [...restaurant.starterMenu,...restaurant.mainMenu]
+// // Boucle for of
+// const menu = [...restaurant.starterMenu,...restaurant.mainMenu]
 
-for (const item of menu)console.log(item);
+// for (const item of menu)console.log(item);
 
-for(const item of menu.entries()){
-    console.log(`${item[0]+1}: ${item[1]}`);
-}
+// for(const item of menu.entries()){
+//     console.log(`${item[0]+1}: ${item[1]}`);
+// }
 // const rest1 = {
 //     name: 'Capri',
 //     // numGuests: 20,
