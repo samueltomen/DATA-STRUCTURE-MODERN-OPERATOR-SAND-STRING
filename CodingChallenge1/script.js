e84a995f3152aa3eb3879dd24973c19f3ee42a64
Dytsaw <tomennana.samuel@outlook.fr>
2023-02-22 17:06:46 +0100

    coding challenge en cours

--- CodingChallenge1/script.js
+++ CodingChallenge1/script.js
@@ -1,251 +1,22 @@
-'use strict';
+/* Créez un tableau joueur pour chaque équipe (variables 'players1' et 'players2')
 
-// Data needed for a later exercise
-const flights =
-    '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+Le premier joueur de chaque tableau est le gardien de but et les autres sont des joueurs de terrain. 
+Pour Bayern Munich (équipe 1), créez une variable ('gk') avec le nom du gardien de but, et un tableau ('fieldPlayers') contenant tous les 10 joueurs de terrain restants.
 
-// Data needed for first part of the section
-const restaurant = {
-    name: 'Classico Italiano',
-    location: 'Via Angelo Tavanti 23, Firenze, Italy',
-    categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+Créez un tableau 'allPlayers' contenant tous les joueurs des deux équipes (22 joueurs).
 
-    openingHours: {
-        thu: {
-            open: 12,
-            close: 22,
-        },
-        fri: {
-            open: 11,
-            close: 23,
-        },
-        sat: {
-            open: 0, // Open 24 hours
-            close: 24,
-        },
-    },
-    order: function (starterIndex, mainIndex) {
-        return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
-    },
+Pendant le jeu, Bayern Munich (équipe 1) a utilisé 3 joueurs de remplacement. Donc créez un nouveau tableau ('players1Final') contenant tous les joueurs d'origine de l'équipe 1 plus 'Thiago', 'Coutinho' et 'Perisic'.
 
-    orderDelivery: function ({
-        starterIndex = 1,
-        mainIndex = 0,
-        time = '20:00',
-        adress = 'Via del Sole,21',
-    }) {
-        console.log(
-            `Order received ! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${adress} at ${time}`
-        );
-    },
+Basé sur l'objet game.odds, créez une variable pour chaque cote (appelée 'team1', 'draw' et 'team2').
 
-    orderPasta: function (ing1, ing2, ing3) {
-        console.log(
-            `Here is your delicious pasta with ${ing1} ${ing2} ${ing3}`
-        );
-    },
-    orderPizza: function (mainIngredient, ...othersIngredients) {
-        console.log(mainIngredient);
-        console.log(othersIngredients);
-    },
-};
+Écrivez une fonction ('printGoals') qui reçoit un nombre arbitraire de noms de joueurs (pas un tableau) et qui les affiche chacun dans la console, ainsi que le nombre total de buts marqués (nombre de noms de joueurs passés en paramètre).
 
-const rest1 = {
-    name: 'Capri',
-    // numGuests: 20,
-    numGuests: 0,
-};
+L'équipe ayant la cote la plus basse a plus de chance de gagner. Imprimez dans la console laquelle des deux équipes a plus de chance de gagner, sans utiliser d'instruction if/else ou l'opérateur ternaire.
 
-const rest2 = {
-    name: 'La Piazza',
-    owner: 'Giovanni Rossi',
-};
+Test data pour 6.: D'abord, utilisez les joueurs 'Davies', 'Muller', 'Lewandowski' et 'Kimmich'.
 
-// // OR assignement operator
-// rest1.numGuests = rest1.numGuests || 10;
-// rest2.numGuests = rest2.numGuests || 10;
+Ensuite, appelez à nouveau la fonction avec des joueurs provenant de game.scored. */
 
-// Simplification de l'operation du dessus (similaire a +=)
-// rest1.numGuests ||= 10;
-// rest2.numGuests ||= 10;
-
-// // nullish assignement operator (null or undefined)
-rest1.numGuests ??= 10;
-rest2.numGuests ??= 10;
-
-// AND assignements operator (retourne la valeur false)
-// rest1.owner = rest1.owner && '<ANONYMOUS>'
-// rest2.owner = rest2.owner && '<ANONYMOUS>'
-// Simplification de l'operation du dessus 
-rest1.owner &&= '<ANONYMOUS>'
-rest2.owner &&= '<ANONYMOUS>'
-
-// console.log(rest1);
-// console.log(rest2);
-
-///////////////////////////////////////////////////
-// The Nullish Coalescing Operator
-// restaurant.numGuests = 0
-// const guest2 = restaurant.numGuests || 10
-// console.log(guest2);
-
-// // Nullish : null and undefined (NOT 0 or '')
-// const guestCorrect = restaurant.numGuests ?? 10
-// console.log(guestCorrect);
-
-// console.log('------- OR -------');
-// // use ANY data types, return ANY data type, short-circuitting
-// // Si la première valeur est veridique il renverra toujours celle ci
-// console.log( 3 || 'Jonas');
-// console.log('' || 'Jonas');
-// console.log(true || 0);
-// console.log(undefined || null);
-// console.log(undefined || 0 || '' || "Hello" || 23 || null);
-
-// const guests1 = restaurant.numGuests ? restaurant.numGuests : 10
-// console.log(guests1);
-
-// restaurant.numGuests = 0
-// const guest2 = restaurant.numGuests || 10
-// console.log(guest2);
-// console.log(restaurant.numGuests);
-
-// console.log('------- AND -------');
-
-// console.log(0 && 'Jonas');
-// console.log(7 && 'Jonas');
-// console.log('Hello' && 23 && null && 'jonas');
-
-// // Practical example
-// if( restaurant.orderPizza){
-//     restaurant.orderPizza('mushroom' ,'spinach')
-// }
-// restaurant.orderPizza && restaurant.orderPizza ('spinach', 'mushromm')
-
-// // 1) Destructuring
-
-// // SPREAD, because on RIGHT side of =
-// const arr5 = [1, 2, ...[3, 4]];
-
-// // REST
-// const [a, b, ...others] = [1, 2, 3, 4, 5];
-// console.log(a, b, ...others);
-
-// const [pizza, , Risotto, ...otherFood] = [
-//     ...restaurant.mainMenu,
-//     ...restaurant.starterMenu,
-// ];
-// console.log(pizza, Risotto, otherFood);
-
-// // Objects
-// const { sat, ...weekDays } = restaurant.openingHours;
-// console.log(weekDays);
-
-// //  2) Functions
-// const add = function (...numbers) {
-//     let sum = 0;
-//     for (let i = 0; i < numbers.length; i++) sum += numbers[i];
-//     console.log(sum);
-// };
-// add(2, 3);
-// add(5, 3, 7, 2);
-
-// const x = [23, 5, 7];
-// add(...x);
-
-// restaurant.orderPizza('mushroom','onions','olives','spinach')
-// restaurant.orderPizza('mushrooms')
-
-// restaurant.orderDelivery({
-//     time: '22:30',
-//     adress: 'Via del Sole,21',
-//     mainIndex: 2,
-//     starterIndex: 2,
-// });
-
-// restaurant.orderDelivery({
-//     adress: '20 rue de la Soif',
-//     mainIndex: 2,
-// });
-
-// const { name, openingHours, categories } = restaurant;
-// console.log(name, openingHours, categories);
-
-// const {
-//     name: restaurantName,
-//     openingHours: hours,
-//     categories: tags,
-// } = restaurant;
-// console.log(restaurantName, hours, tags);
-// // Default values
-// const { menu = [], starterMenu: starters = [] } = restaurant;
-// console.log(menu, starters);
-
-// // Mutating variables
-// let a = 111;
-// let b = 999;
-// const obj = { a: 23, b: 7, c: 14 };
-// ({ a, b } = obj);
-// console.log(a, b);
-
-// // Nested objects
-// const {
-//     fri: { open: o, close: c },
-// } = openingHours;
-// console.log(o, c);
-
-// ----------------------- SPREAD OPERATORS ----------------- //
-
-// Utiliser le spread operator ... (ajoute les valeurs d'un tableau a un autre tableau)
-// const arr = [7, 8, 9];
-// const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
-// console.log(badNewArr);
-
-// const newArr = [1, 2, ...arr];
-// console.log(newArr);
-
-// console.log(...newArr);
-
-// const newMenu = [...restaurant.mainMenu, 'Gnocci'];
-// console.log(newMenu);
-
-// // Copy array
-// const mainMenuCopy = [...restaurant.mainMenu];
-
-// // Join 2 Arrays
-// const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
-// console.log(...menu);
-
-// // Iterables are: arrays, strings, maps, sets but NOT an OBJECT
-// const str = 'Jonas';
-// const letters = [...str, '', '.S'];
-// console.log(...letters);
-// console.log(...str);
-// // console.log(`${...str}Schedtman`); //NOT WORKING
-
-// // Real-world example
-// const ingredients = [
-//     // prompt("Let's make pasta! Ingredient 1 ?"),
-//     // prompt('Ingredient 2?'),
-//     // prompt('Ingredient 3?'),
-// ];
-// console.log(ingredients);
-// restaurant.orderPasta(...ingredients);
-
-// // Object
-// const newRestaurant = { foundedIn: 1998, ...restaurant, founder: 'Guiseppe' };
-// console.log(newRestaurant);
-
-// const restaurantCopy = { ...restaurant };
-// restaurantCopy.name = 'Ristorante Roma';
-// console.log(restaurant.name, restaurantCopy.name);
-
-// Coding Challenge #1
-
-
-// Data of the game from the Web
 const game = {
     team1: 'Bayern Munich',
     team2: 'Borrussia Dortmund',
@@ -278,8 +49,7 @@ const game = {
         ],
     ],
     score: '4:0',
-    scored: ['Lewandowski', 'Gnarby', 'Lewandowski',
-    'Hummels'],
+    scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels','Eddy'],
     date: 'Nov 9th, 2037',
     odds: {
         team1: 1.33,
@@ -288,62 +58,33 @@ const game = {
     },
 };
 
-console.log('--------- DEFINIS LES EQUIPE DANS LES TABLEAU--------------');
-const players1 = game.players[0]
-const players2 = game.players[1]
-console.log(players1);
-console.log(players2);
+const players1 = game.players[0];
+const players2 = game.players[1];
+console.log(...players1);
+console.log(...players2);
 
-console.log('---------- METTRE LE GARDIEN DANS UNE VARIABLE --------------');
-console.log('---- CREER UN TABLEAU AVEC TOUS LES JOUEURS SAUF LE GARDIEN ------');
-
-const [gk,...fieldPlayers] = players1 
+const [gk, ...fieldPlayers] = players1;
 console.log(gk);
-console.log(fieldPlayers);
+console.log(...fieldPlayers);
 
-console.log('------ TABLEAU QUI CONTIENT TOUS LES JOUEURS------');
-const allPlayers = [...players1,...players2]
+const allPlayers = [...players1, ...players2];
 console.log(allPlayers);
 
-// Create new array for the 3 substitute players
-const players1Final = [...players1,'Thiago','Counthino','Perisic']
+const players1Final = [...players1, 'Thiago', 'Counthino', 'Perisic'];
 console.log(players1Final);
 
-// Create a variable for each odd
+const {
+    odds: { team1: team1 },
+    odds: { x: draw },
+    odds: { team2: team2 },
+} = game;
 
-// const {team1, ...tim1} = game.odds
-// const {x, ...draw} = game.odds
-// const {team2, ...tim2} = game.odds
-// console.log(team1);
-// console.log(x);
-// console.log(team2);
+console.log(team1);
+console.log(draw);
+console.log(team2);
 
-// Methode plus simple
-const {odds: {team1,x:draw, team2}} = game
-
-
-function printGoal (...playersName) {
-    console.log(...playersName);
-    console.log(game.score);
+function printGoals(...names) {
+    console.log(...names, game.scored.length); 
 }
 
-printGoal('Davies','Muller','Lewandowski','Kimmich')
-printGoal(...game.scored)
-
-// Elaboration du schema 
-if(team1 < team2){
-    console.log(team1);
-}else{
-    console.log(team2);
-};
-// 7.
-team1 < team2 && console.log('Team 1 is more likely to win');
-team2 < team1 && console.log('Team 2 is more likely to win');
-
-
-
-
-
-
-
-
+printGoals(...game.scored) 
