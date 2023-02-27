a6daab52794ff600290a28ddbbae038fc45608ad
Dytsaw <tomennana.samuel@outlook.fr>
2023-02-27 10:14:39 +0100

    finished script3.js

--- WorkingWithStrings/script3.js
+++ WorkingWithStrings/script3.js
@@ -86,6 +86,9 @@ for (const [i, row] of rows.entries()) {
     // Affichage de la chaîne de caractères créée ainsi que des symboles "✅"
     console.log(`${output.padEnd(20)}${'✅'.repeat(i + 1)}`);
 }
+
+
+
 // Notre solution
 const transformText = function (variable) {
     // Conversion de la chaîne de caractères en minuscules
